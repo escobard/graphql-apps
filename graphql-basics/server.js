cd3c9f6a021134e7c5c31ec04c5437afaa7f758a
Daniel Escobar <escobard90@gmail.com>
2018-04-06 12:30:03 -0600

    feat: added base graphql integration, hooked up the /graphql route to graphqil

--- graphql-basics/server.js
+++ graphql-basics/server.js
@@ -1,6 +1,18 @@
-const express = require('express');
+const express = require('express'),
+expressGraphQL = require('express-graphql')
 
 const app = express()
+
+// the .use() method is intended to BIND middlewares to your application under certain routes
+app.use('/graphql', 
+
+	// grabs the express-graphQL middleware to handle with our routes
+	// this tells express to handle this particular request with the graphQL library
+	expressGraphQL({
+
+		// this tells the application to displaya the graphiql IDE when this route is reached
+		graphiql: true
+	}))
 
 app.listen(4000, () =>{
 	console.log('Listening on port 4000')
