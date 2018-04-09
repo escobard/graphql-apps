512a57049410f0af2719894c8fe04c212866b1b3
Daniel Escobar <escobard90@gmail.com>
2018-04-09 14:11:02 -0600

    feat: added apollo client min config to hookup with react

--- graphql-frontend/client/index.js
+++ graphql-frontend/client/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// grabs the apollo client library
+import ApolloClient from 'apollo-client';
+
+// grabs the apollo provider to hook up the apollo client with react
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+
+})
+
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+  	<ApolloProvider client={client}>
+  		<div>Lyrical</div>
+  	</ApolloProvider>
+  	)
 };
 
 ReactDOM.render(
