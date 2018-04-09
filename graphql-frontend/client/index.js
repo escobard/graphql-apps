ef30da451c04fe70304496edee1fd0c2ef46de4e
Daniel Escobar <escobard90@gmail.com>
2018-04-09 14:31:00 -0600

    feat: added first component to handle songlist rendering

--- graphql-frontend/client/index.js
+++ graphql-frontend/client/index.js
@@ -7,6 +7,11 @@ import ApolloClient from 'apollo-client';
 // grabs the apollo provider to hook up the apollo client with react
 import { ApolloProvider } from 'react-apollo';
 
+import SongList from "./components/SongList"
+
+// apollo requires 0 configuration out of the box to work with your graphql server
+// as long as the /graphql route is setup on the API
+// min config: const client = new ApolloClient({})
 const client = new ApolloClient({
 
 })
@@ -14,7 +19,7 @@ const client = new ApolloClient({
 const Root = () => {
   return (
   	<ApolloProvider client={client}>
-  		<div>Lyrical</div>
+  		<SongList />
   	</ApolloProvider>
   	)
 };
