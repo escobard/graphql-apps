f0fbfa47dfdf31bec63a70ae43f8c0062d1e119c
Daniel Escobar <escobard90@gmail.com>
2018-04-15 19:38:14 -0600

    feat: finished branch, last commit of v0.4

--- graphql-frontend/client/components/SongCreate.js
+++ graphql-frontend/client/components/SongCreate.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 
 class SongCreate extends Component{
+	constructor(props){
+		super(props)
+		this.state = { title: ''}
+	}
+
 	render(){
 		return(
 				<div className="song-create">
 					<h3>Create a new song</h3>
-					<form action=""></form>
+					<form action="">
+						<label htmlFor="">Song Title:</label>
+						<input type="text" value={this.state.title} onChange={event => this.setState({title: event.target.value})} />
+					</form>
 				</div>
 			)
 	}
