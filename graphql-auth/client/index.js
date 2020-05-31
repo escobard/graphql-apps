b2deafddae1d7c2d34fc52c71fe70eba3d5d98b6
Dan <escobard90@gmail.com>
2020-05-31 12:01:12 -0600

    graphql-auth - feat: react router + header

--- graphql-auth/client/index.js
+++ graphql-auth/client/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, hashHistory, Route, IndexRoute } from "react-router";
 
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
+import App from "./components/App";
+
+// TODO - different setup from Cleanover, flag for Jason
 const client = new ApolloClient({
   // identify every record that comes back from the server, So rather than refetching our data for every single query
   // that is issued, Apollo will have the ability to identify the information that its already been pulled down from
@@ -15,9 +19,13 @@ const client = new ApolloClient({
 
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client}>
+      <Router history={hashHistory} >
+        <Route path="/" component={App}>
+
+        </Route>
+      </Router>
+    </ApolloProvider>
   );
 };
 
