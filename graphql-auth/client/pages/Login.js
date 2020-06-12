0fb399b5a3ba7ce8d47406bbdb2c14830df5218a
Dan <escobard90@gmail.com>
2020-06-12 13:42:03 -0600

    graphql-auth - feat: login errors base

--- graphql-auth/client/pages/Login.js
+++ graphql-auth/client/pages/Login.js
@@ -2,14 +2,18 @@ import React, { Component } from "react";
 import { graphql } from "react-apollo";
 
 import AuthForm from "../components/forms/AuthForm";
+import CurrentUserMutation from "../queries/CurrentUser";
 import LoginMutation from "../mutations/Login";
 
 class Login extends Component {
   onSubmit({ email, password }) {
     this.props.mutate({
+      refetchQueries: [{ query: CurrentUserMutation }],
       // https://www.apollographql.com/docs/react/data/mutations/#executing-a-mutation
       // this sets query variables for the mutation
       variables: { email, password }
+    }).catch(e => {
+      const errors = e.graphQLErrors.map(error => error.message);
     });
   }
 
@@ -17,7 +21,7 @@ class Login extends Component {
     return (
       <div className="container">
         <h3>Login</h3>
-        <AuthForm onSubmit={this.onSubmit.bind(this)}/>
+        <AuthForm onSubmit={this.onSubmit.bind(this)} />
       </div>
     );
   }
