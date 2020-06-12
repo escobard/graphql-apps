643f6c4aa18bfaaa2d1de3051d5072416383022a
Dan <escobard90@gmail.com>
2020-06-12 12:29:16 -0600

    graphql-auth - feat: login form w/ gql

--- graphql-auth/client/pages/Login.js
+++ graphql-auth/client/pages/Login.js
@@ -5,11 +5,19 @@ import AuthForm from "../components/forms/AuthForm";
 import LoginMutation from "../mutations/Login";
 
 class Login extends Component {
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      // https://www.apollographql.com/docs/react/data/mutations/#executing-a-mutation
+      // this sets query variables for the mutation
+      variables: { email, password }
+    });
+  }
+
   render() {
     return (
       <div className="container">
         <h3>Login</h3>
-        <AuthForm />
+        <AuthForm onSubmit={this.onSubmit.bind(this)}/>
       </div>
     );
   }
