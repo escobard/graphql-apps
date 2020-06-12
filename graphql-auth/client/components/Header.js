d271570ab9fb16d9763bf9bfb561d6a0a74531d4
Dan <escobard90@gmail.com>
2020-06-12 10:18:14 -0600

    graphql-auth - feat: Header auth base

--- graphql-auth/client/components/Header.js
+++ graphql-auth/client/components/Header.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import query from '../queries/CurrentUser';
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import query from "../queries/CurrentUser";
 
 class Header extends Component {
-  render(){
+  renderButtons() {
+    const { loading, user } = this.props.data;
+
+    if (loading) {
+      return <div />;
+    }
+
+    if (user) {
+      return <div>Logout</div>;
+    } else {
+      return <div>You're not signed in</div>;
+    }
+  }
+
+  render() {
     console.log(this.props.data);
-    return(
-      <div>
-        Header
-      </div>
-    )
+    return (
+      <nav>
+        <div>{this.renderButtons()}</div>
+      </nav>
+    );
   }
 }
 
