8c1ec6bd4896f2f8592f76faabdfe0f5b2f219fa
Dan <escobard90@gmail.com>
2020-06-12 11:21:24 -0600

    graphql-auth - feat: LoginForm base

--- graphql-auth/client/index.js
+++ graphql-auth/client/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { Router, hashHistory, Route, IndexRoute } from "react-router";
 
-import ApolloClient, { createNetworkInterface } from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
+import ApolloClient, { createNetworkInterface } from "apollo-client";
+import { ApolloProvider } from "react-apollo";
 
 import App from "./components/App";
+import LoginForm from "./components/LoginForm";
 
 // apollo assumes the gql client on the server is listening to /graphql, but using a networkInterface removes the
 // implicit behavior, so the uri property is required
 const networkInterface = createNetworkInterface({
-  uri: '/graphql',
+  uri: "/graphql",
   opts: {
     // tells apollo to send along cookies with every query it sends to the server
-    credentials: 'same-origin'
+    credentials: "same-origin"
   }
-})
+});
 
 // TODO - different setup from Cleanover, flag for Jason
 const client = new ApolloClient({
@@ -25,19 +26,19 @@ const client = new ApolloClient({
   // the server and store it inside of some local cache.
   // https://www.apollographql.com/docs/react/caching/cache-configuration/
   // https://www.apollographql.com/docs/react/caching/cache-configuration/#custom-identifiers
-  dataIdFromObject: o => o.id,
+  dataIdFromObject: o => o.id
 });
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <Router history={hashHistory} >
+      <Router history={hashHistory}>
         <Route path="/" component={App}>
-
+           <Route path="login" component={LoginForm} />
         </Route>
       </Router>
     </ApolloProvider>
   );
 };
 
-ReactDOM.render(<Root />, document.querySelector('#root'));
+ReactDOM.render(<Root />, document.querySelector("#root"));
